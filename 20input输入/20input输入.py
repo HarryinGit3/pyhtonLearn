dfb236b11db4d9b924ce1a2ea973b2561c771200
Harryin <yh821219454@163.com>
2017-08-24 16:49:26 +0800

    input输入

---
+++ 20input输入/20input输入.py
@@ -0,0 +1,8 @@
+num =input("please give a number:")
+a_input=int(num)
+if a_input == 1:
+    print("This is a good one")
+elif a_input ==2:
+    print("See you next time")
+else:
+    print("Good luck")
