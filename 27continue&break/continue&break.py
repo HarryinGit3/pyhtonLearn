750ba2e00ee65855fba42a3db7e1960bfdd5fad8
Harryin <yh821219454@163.com>
2017-08-24 20:13:48 +0800

    continue&break

---
+++ 27continue&break/continue&break.py
@@ -0,0 +1,17 @@
+a=True
+while a:
+    b=input('type something')
+    if b == '1':
+        a=False
+    else:
+        pass
+print('finish run')
+a=True
+while True:
+    b=input('type something')
+    if b == '1':
+        continue
+    else:
+        pass
+    print('still')
+print('finish run')
