7184690b2352f5896973477b7d5f3ba1fffb84c9
Harryin <yh821219454@163.com>
2017-08-24 20:00:50 +0800

    import接触

---
+++ 25import载入模块/25import载入模块.py
@@ -0,0 +1,7 @@
+import time
+import time as t
+from time import time,localtime
+from time import * #这样之后 所有的t都可以省去
+print(t.localtime())
+print(t.time())
+print(time()) #因为from time import time 可以省去t
