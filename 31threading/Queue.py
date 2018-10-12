70a8480123553151c40462b57d3ec66827b9830a
Harryin <harryin123@qq.com>
2018-10-12 10:33:32 +0800

    修改

--- 31threading/Queue.py
+++ 31threading/Queue.py
@@ -1,17 +1,19 @@
 import threading
 from queue import Queue
 
-def job(l,q):
+
+def job(l, q):
     for i in range(len(l)):
-        l[i]=l[i]**2;
+        l[i] = l[i] ** 2;
     q.put(l)
+
 
 def multithreading():
     q = Queue()
     threads = []
-    data =[[1,2,3],[2,3,4],[3,4,5],[4,5,6]]
+    data = [[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6]]
     for i in range(4):
-        t=threading.Thread(target=job,args=[data[i],q])
+        t = threading.Thread(target=job, args=[data[i], q])
         t.start()
         threads.append(t)
     for thread in threads:
@@ -21,5 +23,28 @@ def multithreading():
         results.append(q.get())
     print(results)
 
+
 if __name__ == '__main__':
     multithreading()
+    a = [B101	varchar(40)	，
+B102	varchar(32)	,
+B103	varchar(128)	,
+B104	varchar(14)	,
+B105	varchar(14)	,
+B106	varchar(14)	,
+B201	char(2)	,
+B202	json	,
+B203	varchar(2000)	,
+B204	varchar(256)	,
+B205	point	,
+B206	json	,
+B207	varchar(2000)	,
+B208	json	,
+B209	varchar(2000)	,
+B210	json	,
+B211	varchar(2000)	,
+B212	json	,
+B213	varchar(2000)	,
+B214	json	,
+B215	json	,
+         ]
